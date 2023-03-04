35a27169e2394c6e350d22a1b9bdf3665a87e037
Blaser993 <corradodepinto.design@gmail.com>
2023-03-04 15:16:04 +0100

    ok

---
+++ src/components/Monster.vue
@@ -0,0 +1,33 @@
+<template>
+        <li class="monster card">
+          
+          <img :src="monster.card_images[0].image_url" alt="pic">
+                
+          <h2>{{monster.name}}</h2>
+        </li>
+</template>
+  
+  <script>
+  export default{
+    props:{
+      monster: {
+        type: Object,
+        required: true
+      }
+    },
+  }
+  
+  </script>
+    
+  
+  
+  <style lang="scss" scoped>
+     .monster{
+      border: red solid 2px;
+      text-align: center;
+      img{
+        max-height: 600px;
+      }
+    }
+  
+  </style>
