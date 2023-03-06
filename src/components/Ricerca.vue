afaae595a329c59a183d080c9b5da9e7f8e68d5a
Blaser993 <corradodepinto.design@gmail.com>
2023-03-06 15:23:27 +0100

    START

---
+++ src/components/Ricerca.vue
@@ -0,0 +1,26 @@
+<template>
+    <input @keyup.enter="$emit(onClick)" class="cerca" type="text" placeholder="Filtra una carta per nome..." v-model="store.ricerca">
+</template>
+  
+  <script>
+  export default{
+    data(){
+      return{
+        store,
+      },
+    },
+    methods: {
+      onClick(){
+          console.log("ho cercato", this.store.ricerca, "poi" ,this.ricerca)
+        }
+    }
+  }
+  
+  </script>
+    
+  
+  
+  <style lang="scss" scoped>
+  
+  
+  </style>
